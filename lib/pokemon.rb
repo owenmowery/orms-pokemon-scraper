1a3b509aaaecc1b79acd550677d27fd134e43613
Owen Mowery <owenmowery99@gmail.com>
2021-03-27 18:10:23 -0400

    commit tree 14ed24c7536eebfa2d621a0726ecfc1aaedd7929

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+
+    attr_accessor :name, :type, :db, :id
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db_conn)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+
+        db_conn.execute(sql, name, type)
+    end
+
+    def self.find(id, db_conn)
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = ?
+        SQL
+
+        poke = db_conn.execute(sql, id).flatten
+        name = poke[1]
+        type = poke[2]
+        Pokemon.new(id: id, name: name, type: type, db: db_conn)
+    end
 end
